6b833f68bfffe7480f5d62f6c0079da6b6b7d4ff
Bruno Cordoba <brunonahuel87@gmail.com>
2023-10-31 20:35:14 -0300

    agregando if para comparar

---
+++ js/Ejercicio-9.js
@@ -0,0 +1,35 @@
+/* 9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
+Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
+ */ 
+
+let frase = prompt("Ingrese su frase: ");
+let contador = 0;
+let longitud = frase.length;
+let vocalesEncontradas = ''; 
+
+while (contador < longitud) {
+    let caracter = frase.charAt(contador);
+    
+    if (caracter == 'a' || caracter == 'A') {
+        vocalesEncontradas += caracter; 
+    } 
+    else if (caracter == 'e' || caracter == 'E') {
+        vocalesEncontradas += caracter; 
+    } 
+    else if (caracter == 'i' || caracter == 'I') {
+        vocalesEncontradas += caracter; 
+    } 
+    else if (caracter == 'o' || caracter == 'O') {
+        vocalesEncontradas += caracter; 
+    } 
+    else if (caracter == 'u' || caracter == 'U') {
+        vocalesEncontradas += caracter; 
+    } 
+    
+    contador++;
+}
+
+document.write("Las vocales encontradas son: " + vocalesEncontradas);
+
+
+
