b3d5ce01916baf67913834b2b4a415f5a222c851
Bruno Cordoba <brunonahuel87@gmail.com>
2023-10-31 20:06:07 -0300

    termine ejercicio 8

--- js/Ejercicio-8.js
+++ js/Ejercicio-8.js
@@ -1,2 +1,8 @@
-/*8.- Escribe un programa que pida un número y diga si es divisible por 2*/ 
+/*8.- Escribe un programa que pida un número y diga si es divisible por 2*/
 let Numero = parseFloat(prompt("Ingrese su Numero: "));
+
+if (Numero % 2 == 0) {
+  document.write("El numero ingresado si es divisible en 2");
+} else {
+  document.write("El numero ingresado no es divisible en 2");
+}
